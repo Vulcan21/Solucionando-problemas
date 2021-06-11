3e45a2298c4f3dded5d768696ebdd5bf359bab97
Vulcan21 <81696701+Vulcan21@users.noreply.github.com>
2021-06-11 09:02:00 -0500

    Add files via upload

---
+++ primero.js
@@ -0,0 +1,46 @@
+// Mi primera practica con JavaScript.
+//Este es un comentario
+/*Y este un comentario multilinea*/
+// Autor: Juan Daniel Murillo Moreno
+const nombre = 'Juan Daniel' ;
+var tipo_doc = '7654321' ;
+let edad = '13' ;
+console.log (Hola '+nombre+' . Tu numero de documento es '+tipo_doc+'. Tienes: '+edad+');
+let anyos= "13"; 
+let comparacion = edad == anyos;
+console.log (comparacion);
+console.log (edad+anyos);
+console.log (edad+otro);
+console.log (otro>=edad);
+
+// 21/11/2007
+let suma = 0;
+suma = suma + 2;
+console.log(suma);
+
+suma = suma + 1;
+console.log(suma);
+
+suma = suma + 1;
+console.log(suma);
+
+suma = suma + 1;
+console.log(suma);
+
+suma = suma + 2;
+console.log(suma);
+
+suma = suma + 0;
+console.log(suma);
+
+suma = suma + 0;
+console.log(suma);
+
+suma = suma + 7;
+console.log(suma);
+
+var num1=0; num2=0;
+parseInt(prompt("Un cordial saludo, para ingresar un numero dale a la tecla enter"));
+num1=(prompt("ingresa un numero"));
+num2=(prompt("ingresa otro numero"));
+alert (num1+num2) ;
